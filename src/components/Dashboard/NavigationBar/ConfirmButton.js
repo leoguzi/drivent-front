55cf9722c65b3ed7851b0f0a27dbf6e85dd3be0b
Carlos Gabriel <carlosgabrielpc@live.com>
2022-01-21 12:20:09 -0300

    feat: create generic confirm button

---
+++ src/components/Dashboard/NavigationBar/ConfirmButton.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { CircularProgress } from "@material-ui/core";
+
+import Button from "../../Form/Button";
+
+export default function ConfirmButton({ isLoading = false, children, ...props }) {
+  return (
+    <StyledButton {...props} disabled={isLoading}>
+      {isLoading 
+        ? <CircularProgress color="secondary" size={28} disableShrink />
+        : children}
+    </StyledButton>
+  );
+}
+
+const StyledButton = styled(Button)`
+  width: 200px;
+  height: 40px;
+`;
