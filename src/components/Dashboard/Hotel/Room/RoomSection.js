bc4a04bb37d26918bc941c2dc262af9af02aee86
Yasmim Cavalcanti <yasmimcavalcanti.r@gmail.com>
2022-01-21 16:31:01 -0300

    feat: add HotelApi and getHotelRooms method

--- src/components/Dashboard/Hotel/Room/RoomSection.js
+++ src/components/Dashboard/Hotel/Room/RoomSection.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import useApi from "../../../../hooks/useApi";
 import SectionContainer from "../../SectionContainer";
 
 export default function RoomSection({ selectedHotel }) {
+  selectedHotel = 1; //just for development
+
+  const { hotel } = useApi();
+
+  const [rooms, setRooms] = useState(null);
+
+  useEffect(() => {
+    hotel.getHotelRooms(selectedHotel).then(resp => setRooms(resp.data)).catch(error => console.error(error));
+  }, []);
+
   return (
     <SectionContainer title='Ótima pedida! Agora escolha seu quarto'>
       Conteiner de quartos
