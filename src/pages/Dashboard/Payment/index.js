5d572262c4364f90e3d08ee956614c7924fa8ba6
Carlos Gabriel <carlosgabrielpc@live.com>
2022-01-21 01:58:50 -0300

    feat: implement credit card session

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import Cards from "react-credit-cards";
+import InputMask from "react-input-mask";
+import "react-credit-cards/es/styles-compiled.css";
 
 import useApi from "../../../hooks/useApi";
 
 import SectionSubtitle from "../../../components/Dashboard/NavigationBar/SectionSubtitle";
 import SectionTitle from "../../../components/Dashboard/NavigationBar/SectionTitle";
+import Button from "../../../components/Form/Button";
 
 export default function Payment() {
   const [ ticketInfo, setTicketInfo ] = useState(null);
+  const [ creditCardInfo, setCreditCardInfo ] = useState({
+    cvc: "",
+    expiry: "",
+    focus: "",
+    name: "",
+    number: "",
+  });
   const { ticket } = useApi();
 
   useEffect(() => {
@@ -15,6 +26,22 @@ export default function Payment() {
     console.log(data);
     setTicketInfo(data);
   }, []);
+
+  const handleInputFocus = (e) => {
+    setCreditCardInfo({ 
+      ...creditCardInfo,
+      focus: e.target.name
+    });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    
+    setCreditCardInfo({
+      ...creditCardInfo,
+      [name]: value
+    });
+  };
 
   return (
     <>
@@ -30,8 +57,117 @@ export default function Payment() {
         <p>R$ {ticketInfo?.value}</p>
       </TicketInfo>
       <SectionSubtitle>Pagamento</SectionSubtitle>
+      <CreditCardContainer>
+        <Cards
+          cvc={creditCardInfo.cvc}
+          expiry={creditCardInfo.expiry}
+          focused={creditCardInfo.focus}
+          name={creditCardInfo.name}
+          number={creditCardInfo.number}
+        />
+        <InputsCreditCardContainer>
+          <StyledInputMask
+            type="tel"
+            name="number"
+            placeholder="Número do Cartão"
+            mask="9999 9999 9999 9999"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            required
+          />
+          <StyledInputMask
+            type="text"
+            name="name"
+            placeholder="Nome"
+            maxLength={26}
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            required
+          />
+          <RowInputsCreditCard>
+            <StyledInputMask
+              type="tel"
+              name="expiry"
+              placeholder="Validade"
+              mask="99/99"
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+              required
+            />
+            <StyledInputMask
+              type="tel"
+              name="cvc"
+              placeholder="CVC"
+              maxLength={4}
+              onChange={handleInputChange}
+              onFocus={handleInputFocus}
+              required
+            />
+          </RowInputsCreditCard>
+        </InputsCreditCardContainer>
+      </CreditCardContainer>
     </>);
 }
+
+const StyledInputMask = styled(InputMask)`
+  border-radius: 5px;
+  padding-left: 15px;
+  font-size: 20px;
+  outline: none;
+  border: solid 1px #C3C3C3;
+
+  @media (max-width: 770px) {
+    font-size: 15px;
+  }
+`;
+
+const RowInputsCreditCard = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  input {
+    width: 65%;
+  }
+
+  & > input:last-child {
+    width: 30%;
+  }
+`;
+
+const InputsCreditCardContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-left: 15px;
+  padding: 5px;
+
+  & > *:not(:last-child) {
+    margin-bottom: 15px;
+  }
+
+  input {
+    height: 100%;
+  }
+
+  @media (max-width: 770px) {
+    margin-left: 0px;
+  }
+`;
+
+const CreditCardContainer = styled.div`
+  display: flex;
+  max-width: 700px;
+
+  @media (max-width: 770px) {
+    flex-direction: column;
+    max-width: 400px;
+
+    & > div:last-child {
+      margin-top: 20px;
+    }
+  }
+`;
 
 const TicketInfo = styled.div`
   background-color: #FFEED2;
