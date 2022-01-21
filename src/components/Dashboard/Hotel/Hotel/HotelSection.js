7abfcc1d8472126c0c5d31bb8011eca598461c0c
Nello-Moreira <g.moreira1@outlook.com>
2022-01-21 16:39:04 -0300

    feat: error warning on dashboard page

--- src/components/Dashboard/Hotel/Hotel/HotelSection.js
+++ src/components/Dashboard/Hotel/Hotel/HotelSection.js
@@ -2,31 +2,49 @@ import { useState, useEffect } from "react";
 import SectionContainer from "../../SectionContainer";
 import HotelCardsContainer from "./HotelCardsContainer";
 import useApi from "../../../../hooks/useApi";
+import FullDashboardPageWarning from "../../FullDashboardPageWarning";
 
 export default function HotelSection({ selectedHotel, setSelectedHotel }) {
   const [loading, setLoading] = useState(true);
   const [hotels, setHotels] = useState(null);
+  const [error, setError] = useState(null);
   const api = useApi();
 
   useEffect(() => {
+    function errorHandler(error) {
+      if(error.response) {
+        setError({
+          status: error.response.status,
+          message: error.response.data.message
+        });
+      }else if(error.request) {
+        alert("O servidor está fora do ar!");
+      }else{
+        alert("Houve um erro. Por favor, tente novamente.");
+      }
+    }
+
     api.hotel.getAllHotels()
       .then(response => {
         setHotels(response.data);
         setLoading(false);
       })
       .catch(error => {
-        setLoading(false); 
-        alert(error);
+        setLoading(false);
+        
+        errorHandler(error);
       });
   }, []);
       
   return (
-    <SectionContainer title='Primeiro, escolha seu hotel'>
-      {
-        loading
-          ? <p>carregando...</p>
-          : <HotelCardsContainer hotelsArray={hotels}/>
-      }
-    </SectionContainer>
+    loading
+      ? <p>carregando...</p>
+      :error
+        ?<FullDashboardPageWarning message={error.message}/>
+        :(
+          <SectionContainer title='Primeiro, escolha seu hotel'>
+            <HotelCardsContainer hotelsArray={hotels}/>
+          </SectionContainer>
+        )
   );
 }
