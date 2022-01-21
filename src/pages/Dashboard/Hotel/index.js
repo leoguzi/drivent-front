f30440822575fe1cd2a4f956b20c0c4a0640bab3
Nello-Moreira <g.moreira1@outlook.com>
2022-01-21 16:40:52 -0300

    feat: changed dashboard layout and scroll bar

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -3,19 +3,27 @@ import DashboradPageTitle from "../../../components/Dashboard/DashboradPageTitle
 import HotelSection from "../../../components/Dashboard/Hotel/Hotel/HotelSection";
 import RoomSection from "../../../components/Dashboard/Hotel/Room/RoomSection";
 
+import styled from "styled-components";
+
 export default function Hotel() {
   const [selectedHotel, setSelectedHotel] = useState(null);
 	  
   return (
-    <>
+    <Container>
       <DashboradPageTitle>Escolha de hotel e quarto</DashboradPageTitle>
       <HotelSection setSelectedHotel={setSelectedHotel}/>
 
       {
-	  selectedHotel
+	      selectedHotel
           ?<RoomSection selectedHotel={selectedHotel}/>
           :null
       }
-    </>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
