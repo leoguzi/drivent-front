db29f6abf636524202fde70d0117e4e226cfabbc
Carlos Gabriel <carlosgabrielpc@live.com>
2022-01-22 18:35:51 -0300

    feat: implement transition between select ticket and payment sections

--- src/components/PaymentForm/index.js
+++ src/components/PaymentForm/index.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion/dist/framer-motion";
+
 import SelectTicket from "./TicketSelection";
+import Payment from "./Payment";
+import DashboardSlideLeftTransition from "../Dashboard/DashboardSlideLeftTransition";
+import useApi from "../../hooks/useApi";
 
 export default function PaymentForm() {
-  return (<SelectTicket/>);
+  const [isReserved, setIsReserved] = useState(null);
+  const [ticketInfo, setTicketInfo] = useState(false);
+
+  const { ticket } = useApi();
+
+  useEffect(async() => {
+    ticket.getTicketInformation().then((response) => {
+      const { data: ticketInfo } = response;
+      setTicketInfo(ticketInfo);
+      if (ticketInfo) setIsReserved(true);
+    });
+  }, []);
+
+  return (
+    <AnimatePresence>
+      {isReserved
+        ? (
+          <DashboardSlideLeftTransition auxKey={1}>
+            <Payment ticketInfo={ticketInfo}/>
+          </DashboardSlideLeftTransition>
+        )
+        : (
+          <DashboardSlideLeftTransition auxKey={0}>
+            <SelectTicket setIsReserved={setIsReserved} />
+          </DashboardSlideLeftTransition>
+        )
+      }
+    </AnimatePresence>
+  );
 }
