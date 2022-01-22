4135577f0ddb7c20b0f2027257c72684cac53fcf
Leonardo Guzi <leoguzi@hotmail.com>
2022-01-22 14:29:07 -0300

    feat: add withHotel or not selection

--- src/components/PaymentForm/TicketSelection.js
+++ src/components/PaymentForm/TicketSelection.js
@@ -4,11 +4,13 @@ import styled from "styled-components";
 import DashboardPageSubtitle from "../Dashboard/DashboardPageSubtitle";
 import DashboardPageTitle from "../Dashboard/DashboardPageTitle";
 import DashboardWarning from "../Dashboard/DashboardWarning";
+import ConfirmButton from "../../components/Dashboard/NavigationBar/ConfirmButton";
 
 export default function SelectTicket() {
   const [enrollmentStatus, setEnrollmentStatus] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [ticketInfo, setTicketInfo] = useState({ withHotel: false });
+  const [ticketInfo, setTicketInfo] = useState({});
+  const [ticketValue, setTicketValue] = useState(0);
   const { enrollment } = useApi();
 
   useEffect(() => {
@@ -18,7 +20,19 @@ export default function SelectTicket() {
         setIsLoading(false);
       });
   });
+    
+  function handleSubmit() {
         
+  }
+    
+  function updateTicketValue() {
+    const typeValue = ticketInfo.type === "presential" ? 250 : 100; ;
+    const hotelValue = ticketInfo.withHotel ? 350 : 0;
+    setTicketValue(typeValue+hotelValue);
+  }
+
+  useEffect(() => updateTicketValue(), [ticketInfo]);
+  
   return (
     isLoading ? (<p>Carregando...</p>) :
       (enrollmentStatus === 204 ?
@@ -31,7 +45,6 @@ export default function SelectTicket() {
             <DashboardPageSubtitle>Primeiro, escolha sua modalidade de ingresso</DashboardPageSubtitle>
             <OptionsContainer>
               <Option
-                value='presential'
                 onClick={() => setTicketInfo({ ...ticketInfo,  type: "presential" })}
                 selected = {ticketInfo.type === "presential"}
               >
@@ -39,7 +52,6 @@ export default function SelectTicket() {
                 <p>R$ 250</p>
               </Option>
               <Option
-                value='online'
                 onClick={() => setTicketInfo({ ...ticketInfo, type: "online" })}
                 selected = {ticketInfo.type === "online"}
               >           
@@ -47,8 +59,37 @@ export default function SelectTicket() {
                 <p>R$ 100</p>
               </Option>
             </OptionsContainer>
-            <DashboardPageSubtitle>Ótimo! Agora escolha sua modalidade de hospedagem</DashboardPageSubtitle>
-
+            {ticketInfo.type === "presential" &&
+                          (<>
+                            <DashboardPageSubtitle>Ótimo! Agora escolha sua modalidade de hospedagem</DashboardPageSubtitle>
+                            <OptionsContainer>
+                              <Option
+                                onClick={() => setTicketInfo({ ...ticketInfo,  withHotel: false })}
+                                selected = {ticketInfo.withHotel !== undefined && !ticketInfo.withHotel}
+                              >
+                                <p>Sem Hotel</p>
+                                <p>+R$ 0</p>
+                              </Option>
+                              <Option
+                                onClick={() => setTicketInfo({ ...ticketInfo, withHotel: true })}
+                                selected = {ticketInfo.withHotel}
+                              >           
+                                <p>Com Hotel</p>
+                                <p>+R$ 350</p>
+                              </Option>
+                            </OptionsContainer>
+                          </>)
+            }
+            {(ticketInfo.type === "online" || ticketInfo.withHotel !== undefined) &&
+                          (<>
+                            <DashboardPageSubtitle>
+                              Fechado! O total ficou em
+                              <strong> R$ {ticketValue}</strong>.
+                              Agora é só confirmar:
+                            </DashboardPageSubtitle>
+                            <ConfirmButton onClick={() => handleSubmit()}>RESERVAR INGRESSO</ConfirmButton>
+                          </>
+                          )}
           </>)));
 }
 
@@ -64,7 +105,7 @@ align-items: center;
 justify-content: center;
 width: 145px;
 height: 145px;
-border: 1px solid #C8C8C8;
+border: ${(props) => props.selected ? "none": "1px solid #C8C8C8"};
 border-radius: 20px;
 margin-right: 15px;
 background-color: ${(props) => props.selected ? "#FFEED2": "#FFFFFF"};
